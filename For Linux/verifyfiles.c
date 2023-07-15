1b20603d0380adaa3fa0a01721d33c522656fdcd
Gurudatta R K <86517260+GurudattaRK@users.noreply.github.com>
2023-07-16 02:10:02 +0530

    Create verifyfiles.c

---
+++ For Linux/verifyfiles.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    char file1[200], file2[200];
+    printf("Enter first file's name/path to file:\n");
+    scanf("%s",file1);
+    printf("Enter Second file's name/path to file:\n");
+    scanf("%s",file2);
+
+    FILE* reader = fopen(file1,"rb");
+    FILE* reader2 = fopen(file2,"rb");
+    if(reader==NULL)
+    {
+        printf("\nError in opening/finding the file %s",file1);
+        return 0;
+    }
+    if(reader2==NULL)
+    {
+        printf("\nError in opening/finding the file %s",file2);
+        return 0;
+    }
+    unsigned char check=0,in[1],out[1];
+    unsigned long long int count=0;
+
+    printf("Checking files...\n");
+
+    while ((!feof(reader)) && !check)
+    {
+        fread(in,1,1,reader);
+        fread(out,1,1,reader2);
+        check = in[0]^out[0];
+        if(check)
+        {
+            printf("\nERROR \n\nFound at byte %llu",count);
+            goto jump;
+        }
+        count++;
+    }
+    printf("\nNO ERROR\n\n%llu bytes verified",--count);
+    jump:
+    fclose(reader);
+    fclose(reader2);
+    
+}
